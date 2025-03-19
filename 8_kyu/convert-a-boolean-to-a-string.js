5df9d129cfc4c84a9e55d56e55668fdc47f5e9f2
Max Charles II <mcharles2016@yahoo.com>
2025-03-19 12:13:36 -0400

    Convert a boolean to a string codewars solution

---
+++ 8_kyu/convert-a-boolean-to-a-string.js
@@ -0,0 +1,13 @@
+//https://www.codewars.com/kata/551b4501ac0447318f0009cd/train/javascript
+
+function booleanToString(b){
+    //prep
+    //parameter (b for boolean)
+    //return (string)
+    //example true => "true"
+    //pseudocode
+      //enter boolean, string method applied to boolean, boolean becomes string, return result
+    
+    return b.toString();
+    //your code here
+  }
